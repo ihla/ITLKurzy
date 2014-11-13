898a593524123c73e8ad13c6eaecb0b2759cdb11
ihla <lubos.ilcik@gmail.com>
2014-11-13 09:33:33 +0100

    Initial Commit

---
+++ ITLKurzy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ITLKurzy
+//
+//  Created by Lubos Ilcik on 11/13/14.
+//  Copyright (c) 2014 Lubos Ilcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
